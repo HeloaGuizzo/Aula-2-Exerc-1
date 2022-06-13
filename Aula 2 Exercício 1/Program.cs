7c41e2d8c482dc7e6fc4ca6d0296a2b6f0f1f380
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 17:57:25 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 2 Exercício 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int dataAtual = 2022;
+
+            Console.WriteLine("Ano do seu Nascimento: ");
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            int idade = dataAtual - data;
+
+            Console.WriteLine("Sua idade é: " + (idade) + "anos.");
+        }
+    }
+}
